912db216a6663c6f966c39589fcacad230848875
HexeRes <viktoisaev6@gmail.com>
2025-04-18 17:36:58 +0300

    Initial commit

---
+++ app/src/main/java/com/example/projectkt/LoginActivity.java
@@ -0,0 +1,72 @@
+package com.example.projectkt;
+
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class LoginActivity extends AppCompatActivity {
+    private EditText edLogin, edPassword;
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_layout);
+        Init();
+    }
+
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser cUser = mAuth.getCurrentUser();
+        if (cUser != null) {
+            Toast.makeText(this, "пользователь существует", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "пользователь не существует", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void Init() {
+        edLogin = findViewById(R.id.edLogin);
+        edPassword = findViewById(R.id.edPassword);
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    public void onClickSignUp(View view) {
+        if (!TextUtils.isEmpty(edLogin.getText().toString()) && !TextUtils.isEmpty(edPassword.getText().toString())) {
+            mAuth.createUserWithEmailAndPassword(edLogin.getText().toString(), edPassword.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful()){
+                        Toast.makeText(getApplicationContext(), "пользователь успешно зарегестрирован", Toast.LENGTH_SHORT).show();
+                    }
+                    else
+                    {
+                        Toast.makeText(getApplicationContext(), "ошибка", Toast.LENGTH_SHORT).show();
+
+                    }
+
+                }
+            });
+
+
+        }
+
+    }
+
+    public void onClickSignIn(View view) {
+
+    }
+}
